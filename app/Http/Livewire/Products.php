f0a32e41c9bd36fbb6c3ee2a442d4c48ccc9dc8f
Juanchis <juanchismo10@gmail.com>
2022-03-14 07:49:03 -0300

    Creación del método de actualizar productos

--- app/Http/Livewire/Products.php
+++ app/Http/Livewire/Products.php
@@ -12,12 +12,25 @@ class Products extends Component
     public $modal = false;
     public $showEdit = false;
     public $product;
+    public $title, $amount;
+
 
     public function edit($id)
     {
         $this->product = Product::findOrFail($id);
         $this->showEdit = true;
         return view('livewire.edit');
+    }
+
+    // update
+    public function update($product_id)
+    {
+        $product = Product::find($product_id);
+        $product->update([
+            'title' => $this->title,
+            'amount' => $this->amount
+        ]);
+        $this->showEdit = false;
     }
 
     // delete product
--- resources/views/livewire/edit.blade.php
+++ resources/views/livewire/edit.blade.php
@@ -33,20 +33,25 @@
                 <div class="w-full max-w-xs">
                     <form class="bg-white rounded px-8 pt-6 pb-8 mb-4">
                         <div class="mb-4">
+
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                             Nombre
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nombre del producto" value="{{ $product->title }}">
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nombre del producto"
+                        value="{{ $product->title }}"
+                        wire:model="title">
                         </div>
                         <div class="mb-6">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Cantidad
                         </label>
-                        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="Cantidad en stock..." value="{{ $product->amount }}">
+                        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="Cantidad en stock..."
+                        value="{{ $product->amount }}"
+                        wire:model="amount">
 
                         </div>
 
-                        <button type="button" class="bg-green-700 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
+                        <button wire:click="update({{$product->id}})" type="button" class="bg-green-700 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
                             Guardar
                         </button>
 
